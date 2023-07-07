2c5c011379637bf3845426af42809e2abbd339c7
gourabb820 <91730250+gourabb820@users.noreply.github.com>
2023-07-07 17:29:52 +0530

    adding logo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
 import UploadFile from './components/uploadFile/UploadFile';
-import NavBar from './components/Navbar/NavBar';
+import NavBar from './components/navbar/NavBar';
 
 const App = () => {
   return (
     <div>
-      <NavBar title="Upload a file to AWS S3 and store the letter count to  RDS through Lambda"/>
+      <NavBar title="Upload a file to AWS S3 and store the letter count to RDS through Lambda"/>
       <UploadFile />
     </div>
   );

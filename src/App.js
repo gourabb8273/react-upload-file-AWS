8ea9e01da0e5faa8132c19701882d8f62f763678
gourabb820 <91730250+gourabb820@users.noreply.github.com>
2023-07-11 23:52:16 +0530

    changing route

--- src/App.js
+++ src/App.js
@@ -5,13 +5,12 @@ import UploadFile from './components/uploadFile/UploadFile';
 
 const App = () => {
   return (
-    <UploadFile />
-  //   <Router  basename="/upload">
-  //   <Routes>
-  //     <Route path="/upload" element={<UploadFile />} />
-  //     <Route path="/" element={<h3>Welcome to Upload APP</h3>} />
-  //   </Routes>
-  // </Router>
+    <Router  basename="/upload"> 
+    <Routes>
+      <Route path="/" element={<UploadFile />} />
+      {/* <Route path="/" element={<h3>Welcome to Upload APP</h3>} /> */}
+    </Routes>
+  </Router>
   );
 };
 

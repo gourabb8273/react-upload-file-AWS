ba777db5cd660f23ee32540ec86b700772671b1b
gourabb820 <91730250+gourabb820@users.noreply.github.com>
2023-07-12 13:05:22 +0530

    removing route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 import UploadFile from './components/uploadFile/UploadFile';
 
 const App = () => {
   return (
-    <UploadFile />
-  //   <Router  basename="/upload"> 
-  //   <Routes>
-  //     <Route path="/" element={<UploadFile />} />
-  //     {/* <Route path="/" element={<h3>Welcome to Upload APP</h3>} /> */}
-  //   </Routes>
-  // </Router>
+    <UploadFile /> 
   );
 };
 
